da19a1f2f95a70cb12bbf75ac781e74963966345
Chenyang Meng <sunnym412@gmail.com>
2018-04-18 02:20:05 -0500

    read the data

--- PS1/parse.py
+++ PS1/parse.py
@@ -3,22 +3,22 @@ import csv
 data_file = 'house_votes_84.data'
 
 def parse(filename):
-  '''
-  takes a filename and returns attribute information and all the data in array of dictionaries
-  '''
-  # initialize variables
+    '''
+    takes a filename and returns attribute information and all the data in array of dictionaries
+    '''
+    # initialize variables
 
-  out = []  
-  csvfile = open(filename,'rb')
-  fileToRead = csv.reader(csvfile)
+    out = []
+    csvfile = open(filename,'rb')
+    fileToRead = csv.reader(csvfile)
 
-  headers = fileToRead.next()
+    headers = fileToRead.next()
 
-  # iterate through rows of actual data
-  for row in fileToRead:
-    out.append(dict(zip(headers, row)))
+    # iterate through rows of actual data
+    for row in fileToRead:
+      out.append(dict(zip(headers, row)))
 
-  return out
+    return out
 
 examples = parse(data_file)
 #print(type(data))
